9d65415830ef597321b937e9e24d8fcbf379e939
wian adiyatma bisma razaak <croonzbow123@gmail.com>
2025-05-19 22:47:34 +0700

    Menambahkan fitur dark mode

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toogleMenu() {
     menu.classList.toggle('open')
     icon.classList.toggle('open')
 }
+
+const body = document.body
+const darkMods = document.querySelector('.darkMods')
+const icons = document.querySelectorAll('i')
+const changeTheme = document.getElementById('changeTheme')
+body.classList.remove('dark'); // Add instead of remove
+const linkedIn = document.querySelector('.fa-linkedin')
+const down = document.getElementById('scrollDown')
+const up = document.getElementById('ScrollUp')
+const about = document.getElementById('about')
+
+darkMods.addEventListener('click', () => {
+    body.classList.toggle("dark");
+    icons.forEach(icon => {
+        if (body.classList.contains("dark")) {
+            changeTheme.classList.remove("fa-sun");
+                changeTheme.classList.add("fa-moon");
+                icon.style.color = "#fff"
+            } else {
+                changeTheme.classList.remove("fa-moon");
+                changeTheme.classList.add("fa-sun");
+                icon.style.color = "#000"
+            }
+    });
+    })
+
